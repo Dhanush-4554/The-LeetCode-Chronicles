0836b077ed8091fe25f611c247cdb9008a3be5f7
Dhanush <118877804+Dhanush-4554@users.noreply.github.com>
2023-03-10 20:25:23 +0530

    Create 724. Find Pivot Index.cpp

---
+++ 724. Find Pivot Index.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int final =0;
+        for(int i=0;i<nums.size();i++)
+            final  = final  + nums[i];
+
+        int sum=0;
+
+        for(int i=0;i<nums.size();i++){
+            sum = sum + nums[i];
+            if(sum == (final  - (sum -nums[i])))
+                return i;
+        }
+        return -1;
+    }
+};
